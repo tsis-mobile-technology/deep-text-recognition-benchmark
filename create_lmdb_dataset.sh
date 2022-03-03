726cf93359d7902c9a94cd9a2b9d548519be767d
taejong go <gainworld@gmail.com>
2022-03-03 22:50:42 +0900

    lmdb create shell

--- create_lmdb_dataset.sh
+++ create_lmdb_dataset.sh
@@ -2,33 +2,33 @@
 
 #sentence
 python create_lmdb_dataset.py \
---inputPath /home/proidea/jupyter/aihub_study_data/korean_spelling_save_image/sentence/train \
+--inputPath /home/proidea/jupyter/aihub_study_data/korean_spelling_save_image/sentence/ \
 --gtFile /home/proidea/jupyter/aihub_study_data/korean_spelling_save_image/sentence/gt_train.txt \
 --outputPath /home/proidea/jupyter/aihub_study_data/data_lmdb/sentence/train
 
 python create_lmdb_dataset.py \
---inputPath /home/proidea/jupyter/aihub_study_data/korean_spelling_save_image/sentence/validation \
+--inputPath /home/proidea/jupyter/aihub_study_data/korean_spelling_save_image/sentence/ \
 --gtFile /home/proidea/jupyter/aihub_study_data/korean_spelling_save_image/sentence/gt_validation.txt \
 --outputPath /home/proidea/jupyter/aihub_study_data/data_lmdb/sentence/train
 
 #syllable
 python create_lmdb_dataset.py \
---inputPath /home/proidea/jupyter/aihub_study_data/korean_spelling_save_image/syllable/train \
+--inputPath /home/proidea/jupyter/aihub_study_data/korean_spelling_save_image/syllable/ \
 --gtFile /home/proidea/jupyter/aihub_study_data/korean_spelling_save_image/syllable/gt_train.txt \
 --outputPath /home/proidea/jupyter/aihub_study_data/data_lmdb/syllable/train
 
 python create_lmdb_dataset.py \
---inputPath /home/proidea/jupyter/aihub_study_data/korean_spelling_save_image/syllable/validation \
+--inputPath /home/proidea/jupyter/aihub_study_data/korean_spelling_save_image/syllable/ \
 --gtFile /home/proidea/jupyter/aihub_study_data/korean_spelling_save_image/syllable/gt_validation.txt \
 --outputPath /home/proidea/jupyter/aihub_study_data/data_lmdb/syllable/train
 
 #word
 python create_lmdb_dataset.py \
---inputPath /home/proidea/jupyter/aihub_study_data/korean_spelling_save_image/word/train \
+--inputPath /home/proidea/jupyter/aihub_study_data/korean_spelling_save_image/word/ \
 --gtFile /home/proidea/jupyter/aihub_study_data/korean_spelling_save_image/word/gt_train.txt \
 --outputPath /home/proidea/jupyter/aihub_study_data/data_lmdb/word/train
 
 python create_lmdb_dataset.py \
---inputPath /home/proidea/jupyter/aihub_study_data/korean_spelling_save_image/word/validation \
+--inputPath /home/proidea/jupyter/aihub_study_data/korean_spelling_save_image/word/ \
 --gtFile /home/proidea/jupyter/aihub_study_data/korean_spelling_save_image/word/gt_validation.txt \
 --outputPath /home/proidea/jupyter/aihub_study_data/data_lmdb/word/train

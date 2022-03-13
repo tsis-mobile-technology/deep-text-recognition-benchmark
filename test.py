7d96f1dd553096b9fc1ee2968f518372115e30a8
taejong go <gainworld@gmail.com>
2022-03-13 13:45:59 +0900

    modify

--- test.py
+++ test.py
@@ -205,7 +205,8 @@ def test(opt):
 
     # load model
     print('loading pretrained model from %s' % opt.saved_model)
-    model.load_state_dict(torch.load(opt.saved_model, map_location=device))
+    #model.load_state_dict(torch.load(opt.saved_model, map_location=device), strict=False)
+    model.load_state_dict(torch.load(opt.saved_model), strict=False)
     opt.exp_name = '_'.join(opt.saved_model.split('/')[1:])
     # print(model)
 
